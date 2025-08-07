7296687fed8823d22bb2823fd70b0a292f54ad24
Moesen <gustavmoesmand@proton.me>
2025-08-07 08:48:52 +0200

    Add mdformat to format markdown files

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -27,6 +27,7 @@ return {
 			json = { "jq" },
 			jsonc = { "jq" },
 			jsonl = { "jq" },
+			markdown = { "mdformat" },
 			rust = { "rustfmt", lst_format = "fallback" },
 			svelte = { "prettierd" },
 			["_"] = { "trim_whitespace" },
