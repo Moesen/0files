bd8085d28fad4ed83aa84d03de3549a298ecd0fe
Moesen <gustavmoesmand@proton.me>
2025-09-05 12:10:45 +0200

    Change colorscheme to kanso_inc

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,20 @@
 return {
-  {
-    "rebelot/kanagawa.nvim",
-    lazy = false,
-    priority = 1000,
-    name = "kanagawa",
-    config = function()
-      vim.cmd([[colorscheme kanagawa]])
-    end,
-  }
+	-- {
+	-- 	"rebelot/kanagawa.nvim",
+	-- 	lazy = false,
+	-- 	priority = 1000,
+	-- 	name = "kanagawa",
+	-- 	config = function()
+	-- 		vim.cmd([[colorscheme kanagawa]])
+	-- 	end,
+	-- },
+	{
+		"webhooked/kanso.nvim",
+		lazy = false,
+		priority = 1000,
+		name = "kanso",
+		config = function()
+			vim.cmd([[colorscheme kanso]])
+		end,
+	},
 }
