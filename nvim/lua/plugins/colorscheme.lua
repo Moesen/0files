b3c00f3dbdf2711af7fae71a1418b23ef5c511f7
Moesen <gustavmoesmand@proton.me>
2025-09-11 14:07:22 +0200

    Add some stuff

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,17 +1,15 @@
 return {
-	{
-		"webhooked/kanso.nvim",
-		lazy = false,
-		priority = 1000,
-		name = "kanso",
-		config = function()
-			require("kanso").setup({
-				background = {
-					dark = "mist",
-					light = "pearl",
-				},
-			})
-			vim.cmd([[colorscheme kanso]])
-		end,
-	},
+	"webhooked/kanso.nvim",
+	lazy = false,
+	priority = 1000,
+	name = "kanso",
+	config = function()
+		require("kanso").setup({
+			background = {
+				dark = "mist",
+				light = "pearl",
+			},
+			vim.cmd([[colorscheme kanso]]),
+		})
+	end,
 }
