923508b5df09c9df3eb09b46886b1128e7664734
Moesen <gustavmoesmand@proton.me>
2025-09-29 09:40:33 +0200

    Add some stuff

--- zsh/autocompletions/aws.zsh
+++ zsh/autocompletions/aws.zsh
@@ -1,1 +1,6 @@
-[[ -f /usr/local/bin/aws_completer ]] && complete -C '/usr/local/bin/aws_completer' aws
+[[ -f /usr/local/bin/aws_completer ]] && complete -C '/usr/local/bin/aws_completer' awS
+if [[ -f ~/.local/share/amazon-q/shell/zshrc.pre.zsh ]]
+then
+    source ~/.local/share/amazon-q/shell/zshrc.pre.zsh
+    source ~/.local/share/amazon-q/shell/zshrc.post.zsh
+fi
