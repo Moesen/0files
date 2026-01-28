323c05dda0b241129dc22b0cdbc1bfbccca9c2ec
Moesen <gustavmoesmand@proton.me>
2026-01-28 14:42:31 +0100

    Change theme to zenbones

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,15 +1,29 @@
+-- return {
+-- 	"webhooked/kanso.nvim",
+-- 	lazy = false,
+-- 	priority = 1000,
+-- 	name = "kanso",
+-- 	config = function()
+-- 		require("kanso").setup({
+-- 			background = {
+-- 				dark = "mist",
+-- 				light = "pearl",
+-- 			},
+-- 			vim.cmd([[colorscheme kanso]]),
+-- 		})
+-- 	end,
+-- }
 return {
-	"webhooked/kanso.nvim",
+	"zenbones-theme/zenbones.nvim",
+	-- Optionally install Lush. Allows for more configuration or extending the colorscheme
+	-- If you don't want to install lush, make sure to set g:zenbones_compat = 1
+	-- In Vim, compat mode is turned on as Lush only works in Neovim.
+	dependencies = "rktjmp/lush.nvim",
 	lazy = false,
 	priority = 1000,
-	name = "kanso",
+	-- you can set set configuration options here
 	config = function()
-		require("kanso").setup({
-			background = {
-				dark = "mist",
-				light = "pearl",
-			},
-			vim.cmd([[colorscheme kanso]]),
-		})
+		vim.g.zenbones_darken_comments = 45
+		vim.cmd.colorscheme("zenbones")
 	end,
 }
