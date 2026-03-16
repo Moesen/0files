0a36b773d40314d8aea5078d5554aa13cc502dac
Moesen <gustavmoesmand@proton.me>
2026-03-16 10:23:02 +0100

    Change to light color backgrounds

--- hypr/randomize_all_wallpapers.sh
+++ hypr/randomize_all_wallpapers.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-WALLPAPER_DIR="$HOME/0files/wallpapers/dark/"
+WALLPAPER_DIR="$HOME/0files/wallpapers/light/"
 CURRENT_WALL=$(hyprctl hyprpaper listloaded)
 
 for monitor in $(hyprctl monitors -j | jq -r '.[] | .name'); do
