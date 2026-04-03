23b973427a2ae54b8cd268efe401e7ac083e31d5
Moesen <gustav@moesen.dk>
2026-04-03 17:41:44 +0200

    Update ts_ls setup

--- nvim/lua/lsp-configure.lua
+++ nvim/lua/lsp-configure.lua
@@ -34,6 +34,29 @@ vim.lsp.config("ty", {
     },
 })
 
+local vue_language_server_path = vim.fn.stdpath("data")
+    .. "/mason/packages/vue-language-server/node_modules/@vue/language-server"
+
+vim.lsp.config("ts_ls", {
+    filetypes = {
+        "javascript",
+        "javascriptreact",
+        "typescript",
+        "typescriptreact",
+        "vue",
+    },
+    init_options = {
+        hostInfo = "neovim",
+        plugins = {
+            {
+                name = "@vue/typescript-plugin",
+                location = vue_language_server_path,
+                languages = { "vue" },
+            },
+        },
+    },
+})
+
 vim.api.nvim_create_autocmd("LspAttach", {
     group = vim.api.nvim_create_augroup("lsp_attach_disable_ruff_hover", { clear = true }),
     callback = function(args)
