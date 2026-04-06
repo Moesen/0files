fef4b28a013d1d54cbd11e51d30261060e3d542c
Moesen <gustavmoesmand@proton.me>
2026-04-06 10:23:48 +0200

    Make 0files more linux/mac cross-compatible

--- zsh/.zsh_plugins.zsh
+++
@@ -1,7 +0,0 @@
-fpath+=( "$HOME/Library/Caches/antidote/github.com/zsh-users/zsh-syntax-highlighting" )
-source "$HOME/Library/Caches/antidote/github.com/zsh-users/zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh"
-fpath+=( "$HOME/Library/Caches/antidote/github.com/zsh-users/zsh-autosuggestions" )
-source "$HOME/Library/Caches/antidote/github.com/zsh-users/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh"
-fpath+=( "$HOME/Library/Caches/antidote/github.com/zsh-users/zsh-completions" )
-fpath+=( "$HOME/Library/Caches/antidote/github.com/zsh-users/zsh-history-substring-search" )
-source "$HOME/Library/Caches/antidote/github.com/zsh-users/zsh-history-substring-search/zsh-history-substring-search.plugin.zsh"
--- zsh/.zshenv
+++ zsh/.zshenv
@@ -1,8 +1,13 @@
-# Mac specific for antidote
-source /opt/homebrew/opt/antidote/share/antidote/antidote.zsh
-
 # Zsh Config Home
 export ZDOTDIR="${HOME}/0files/zsh"
+
+# Antidote: use Homebrew install on macOS, local submodule elsewhere
+if [[ -f /opt/homebrew/opt/antidote/share/antidote/antidote.zsh ]]; then
+  source /opt/homebrew/opt/antidote/share/antidote/antidote.zsh
+elif [[ -f /usr/local/opt/antidote/share/antidote/antidote.zsh ]]; then
+  source /usr/local/opt/antidote/share/antidote/antidote.zsh
+fi
+# On Linux, antidote is loaded via fpath in .zshrc using the local submodule
 
 # uv
 export PATH="${HOME}/.local/bin:$PATH"
