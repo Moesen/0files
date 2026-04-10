9dd155c5e011006b9b8f512dca2a23d85511bea1
Moesen <gustav@supplios.com>
2026-04-10 11:47:29 +0200

    Add grapple for bookmarking files

---
+++ nvim/lua/plugins/grapple.lua
@@ -0,0 +1,39 @@
+return {
+	{
+		"cbochs/grapple.nvim",
+		event = { "BufReadPost", "BufNewFile" },
+		cmd = "Grapple",
+		dependencies = {
+			{ "nvim-tree/nvim-web-devicons", lazy = true },
+		},
+		init = function()
+			require("which-key").add({ "<leader>h", group = "Bookmarks" })
+		end,
+		opts = {
+			scope = "git",
+		},
+		keys = {
+			{
+				"<leader>hm",
+				function()
+					require("grapple").toggle()
+				end,
+				desc = "Toggle bookmark",
+			},
+			{
+				"<leader>hh",
+				function()
+					require("grapple").toggle_tags()
+				end,
+				desc = "Show bookmarks",
+			},
+			{
+				"<leader>hr",
+				function()
+					require("grapple").untag()
+				end,
+				desc = "Remove bookmark",
+			},
+		},
+	},
+}
