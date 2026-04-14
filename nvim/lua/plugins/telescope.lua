ec174ab974dc079c49f16de010f258dc6935f059
Moesen <gustav@moesen.dk>
2026-04-14 10:07:14 +0200

    Made some changes

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -7,6 +7,99 @@ local function grep_selected_text()
 	})
 end
 
+local function loaded_todo_picker(opts)
+	opts = opts or {}
+
+	require("todo-comments")
+
+	local Config = require("todo-comments.config")
+	local Highlight = require("todo-comments.highlight")
+	local pickers = require("telescope.pickers")
+	local finders = require("telescope.finders")
+	local conf = require("telescope.config").values
+	local make_entry = require("telescope.make_entry")
+
+	local bufs = {}
+	if opts.current_buffer then
+		bufs = { vim.api.nvim_get_current_buf() }
+	else
+		for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+			if vim.api.nvim_buf_is_valid(buf) and vim.api.nvim_buf_is_loaded(buf) then
+				local name = vim.api.nvim_buf_get_name(buf)
+				local buftype = vim.bo[buf].buftype
+				if name ~= "" and buftype == "" then
+					table.insert(bufs, buf)
+				end
+			end
+		end
+	end
+
+	local results = {}
+	for _, buf in ipairs(bufs) do
+		local filename = vim.api.nvim_buf_get_name(buf)
+		local lines = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
+
+		for idx, line in ipairs(lines) do
+			local start_col, _, kw = Highlight.match(line)
+			if start_col and kw then
+				if Config.options.highlight.comments_only and not Highlight.is_comment(buf, idx - 1, start_col - 1) then
+					kw = nil
+				end
+			end
+
+			if kw then
+				table.insert(results, string.format("%s:%d:%d:%s", filename, idx, start_col, line))
+			end
+		end
+	end
+
+	if vim.tbl_isempty(results) then
+		vim.notify("No todos found", vim.log.levels.INFO)
+		return
+	end
+
+	local entry_maker = make_entry.gen_from_vimgrep(opts)
+	pickers
+		.new(opts, {
+			prompt_title = opts.current_buffer and "Todo (Current File)" or "Todo (Loaded Buffers)",
+			finder = finders.new_table({
+				results = results,
+				entry_maker = function(line)
+					local entry = entry_maker(line)
+					entry.display = function(item)
+						local display = string.format("%s:%s:%s ", item.filename, item.lnum, item.col)
+						local text = item.text
+						local start_col, end_col, kw = Highlight.match(text)
+						local hl = {}
+
+						if start_col and end_col and kw then
+							kw = Config.keywords[kw] or kw
+							local icon = Config.options.keywords[kw].icon or " "
+							display = icon .. " " .. display
+							table.insert(hl, { { 0, #icon + 1 }, "TodoFg" .. kw })
+							text = vim.trim(text:sub(start_col))
+							table.insert(hl, {
+								{ #display, #display + end_col - start_col + 2 },
+								"TodoBg" .. kw,
+							})
+							table.insert(hl, {
+								{ #display + end_col - start_col + 1, #display + end_col + 1 + #text },
+								"TodoFg" .. kw,
+							})
+							display = display .. " " .. text
+						end
+
+						return display, hl
+					end
+					return entry
+				end,
+			}),
+			previewer = conf.grep_previewer(opts),
+			sorter = conf.generic_sorter(opts),
+		})
+		:find()
+end
+
 return {
 	"nvim-telescope/telescope.nvim",
 	tag = "0.1.8",
@@ -19,6 +112,20 @@ return {
 	},
 	keys = {
 		{ "<leader>pf", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
+		{
+			"<leader>pt",
+			function()
+				loaded_todo_picker({ current_buffer = true })
+			end,
+			desc = "Todo in current file",
+		},
+		{
+			"<leader>pT",
+			function()
+				loaded_todo_picker()
+			end,
+			desc = "Todo in loaded buffers",
+		},
 		{
 			"<leader>pg",
 			function()
